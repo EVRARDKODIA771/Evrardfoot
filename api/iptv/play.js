159680cb9db7ea086063981915727b28e5084b6e
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-11 18:46:20 +0000

    Update play.js

--- api/iptv/play.js
+++ api/iptv/play.js
@@ -2,13 +2,27 @@ const IPTV_DNS = process.env.IPTV_DNS;
 const IPTV_USERNAME = process.env.IPTV_USERNAME;
 const IPTV_PASSWORD = process.env.IPTV_PASSWORD;
 
+const ACCESS_PASSWORD = "14082022";
+
 export default async function handler(req, res) {
   try {
-    const { stream_id } = req.query;
+    const { stream_id, password } = req.query;
+
+    if (password !== ACCESS_PASSWORD) {
+      return res.status(403).json({
+        error: "Accès refusé",
+      });
+    }
 
     if (!stream_id) {
       return res.status(400).json({
         error: "stream_id manquant",
+      });
+    }
+
+    if (!IPTV_DNS || !IPTV_USERNAME || !IPTV_PASSWORD) {
+      return res.status(500).json({
+        error: "Configuration IPTV manquante",
       });
     }
 
@@ -17,11 +31,14 @@ export default async function handler(req, res) {
     const targetUrl =
       `${base}/live/${IPTV_USERNAME}/${IPTV_PASSWORD}/${stream_id}.m3u8`;
 
-    console.log("Redirect vers :", targetUrl);
+    console.log("PLAY REQUEST");
+    console.log("STREAM:", stream_id);
+    console.log("REDIRECT:", targetUrl);
 
     return res.redirect(302, targetUrl);
+
   } catch (err) {
-    console.error(err);
+    console.error("PLAY ERROR:", err);
 
     return res.status(500).json({
       error: err.message,

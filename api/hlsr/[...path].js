a54577cdaf19e941c03b489eca53258ff7388994
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-11 16:18:39 +0000

    Update [...path].js

--- api/hlsr/[...path].js
+++ api/hlsr/[...path].js
@@ -1,18 +1,6 @@
-const IPTV_DNS = process.env.IPTV_DNS;
-
-export default async function handler(req, res) {
-  try {
-    const { path } = req.query;
-
-    return res.status(200).json({
-      routeWorks: true,
-      path,
-      base: IPTV_DNS
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message
-    });
-  }
+export default function handler(req, res) {
+  return res.status(200).json({
+    url: req.url,
+    query: req.query,
+  });
 }

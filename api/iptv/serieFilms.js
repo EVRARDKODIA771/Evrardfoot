a7a321395a1abea906f6f0b7a541d53ea9c6cd5b
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-13 17:57:01 +0000

    Create serieFilms.js

---
+++ api/iptv/serieFilms.js
@@ -0,0 +1,20 @@
+const IPTV_DNS = process.env.IPTV_DNS;
+const IPTV_USERNAME = process.env.IPTV_USERNAME;
+const IPTV_PASSWORD = process.env.IPTV_PASSWORD;
+
+export default async function handler(req, res) {
+
+  const url =
+    `${IPTV_DNS}/player_api.php` +
+    `?username=${IPTV_USERNAME}` +
+    `&password=${IPTV_PASSWORD}` +
+    `&action=get_vod_streams`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  res.status(200).json(
+    data.slice(0, 20)
+  );
+}

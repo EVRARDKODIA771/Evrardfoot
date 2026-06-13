a75d5e2365978e178f37e7d5bf6c221527b1ac04
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-13 18:11:20 +0000

    Update serieFilms.js

--- api/iptv/serieFilms.js
+++ api/iptv/serieFilms.js
@@ -6,16 +6,17 @@ const IPTV_PASSWORD = process.env.IPTV_PASSWORD;
 
 export default async function handler(req, res) {
 
-const url =
-  `${IPTV_DNS}/player_api.php` +
-  `?username=${IPTV_USERNAME}` +
-  `&password=${IPTV_PASSWORD}` +
-  `&action=get_series_info` +
-  `&series_id=53487`;
+  const url =
+    `${IPTV_DNS}/player_api.php` +
+    `?username=${IPTV_USERNAME}` +
+    `&password=${IPTV_PASSWORD}` +
+    `&action=get_series_info` +
+    `&series_id=53487`;
 
   const response = await fetch(url);
 
-  const data = await response.json();
+  const text = await response.text();
 
-  res.status(200).json(data.slice(0, 20));
+  res.status(200).send(text);
+
 }

6713a2d9cf8579199ae33221d90c2f2668a1f517
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-13 22:22:32 +0000

    Update serieFilms.js

--- api/iptv/serieFilms.js
+++ api/iptv/serieFilms.js
@@ -1,26 +1,28 @@
-// api/iptv/testLatest.js
-
 const IPTV_DNS = process.env.IPTV_DNS;
 const IPTV_USERNAME = process.env.IPTV_USERNAME;
 const IPTV_PASSWORD = process.env.IPTV_PASSWORD;
 
 export default async function handler(req, res) {
 
-  const base = IPTV_DNS.replace(/\/+$/, "");
+  const base =
+    IPTV_DNS.replace(/\/+$/, "");
 
   const url =
     `${base}/player_api.php` +
     `?username=${IPTV_USERNAME}` +
     `&password=${IPTV_PASSWORD}` +
-    `&action=get_vod_streams` +
-    `&category_id=390`;
+    `&action=get_series` +
+    `&category_id=654`;
 
-  const response = await fetch(url);
+  const response =
+    await fetch(url);
 
-  const data = await response.json();
+  const data =
+    await response.json();
 
   res.status(200).json({
     total: data.length,
     sample: data.slice(0, 20)
   });
+
 }

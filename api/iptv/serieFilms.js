258a348f34321b312db56f0d6adeab3135b1077f
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-15 11:07:03 +0000

    Update serieFilms.js

--- api/iptv/serieFilms.js
+++ api/iptv/serieFilms.js
@@ -2,35 +2,19 @@ const IPTV_DNS = process.env.IPTV_DNS;
 const IPTV_USERNAME = process.env.IPTV_USERNAME;
 const IPTV_PASSWORD = process.env.IPTV_PASSWORD;
 
-const MOVIE_CATEGORIES = [
-  "390",
-  "1473",
-  "432",
-  "440",
-  "485",
-  "477",
-  "479",
-  "854",
-  "847",
-  "842",
-  "855",
-  "843",
-  "853",
-  "850",
-  "1281",
-  "867",
-  "856",
-  "868",
-  "858",
-  "866",
-  "860",
-  "585",
-  "848",
-  "862",
-  "852",
-  "859",
-  "833",
-  "834"
+const SERIES_CATEGORIES = [
+  "654",
+  "659",
+  "666",
+  "445",
+  "1337",
+  "1338",
+  "1379",
+  "660",
+  "656",
+  "665",
+  "664",
+  "1386"
 ];
 
 export default async function handler(req, res) {
@@ -43,7 +27,7 @@ export default async function handler(req, res) {
       `${base}/player_api.php` +
       `?username=${IPTV_USERNAME}` +
       `&password=${IPTV_PASSWORD}` +
-      `&action=get_vod_streams`;
+      `&action=get_series`;
 
     const response =
       await fetch(url);
@@ -51,26 +35,27 @@ export default async function handler(req, res) {
     const data =
       await response.json();
 
-    const movies =
+    const series =
       (Array.isArray(data) ? data : [])
-        .filter(movie =>
-          MOVIE_CATEGORIES.includes(
-            String(movie.category_id)
+        .filter(serie =>
+          SERIES_CATEGORIES.includes(
+            String(serie.category_id)
           )
         )
-        .map(movie => ({
-          type: "movie",
-          name: movie.name,
-          stream_id: movie.stream_id,
-          category_id: movie.category_id,
-          logo: movie.stream_icon || "",
-          container_extension:
-            movie.container_extension || "mp4"
+        .map(serie => ({
+          type: "series",
+          name: serie.name,
+          series_id: serie.series_id,
+          category_id: serie.category_id,
+          logo:
+            serie.cover ||
+            serie.stream_icon ||
+            ""
         }));
 
     return res.status(200).json({
-      total: movies.length,
-      movies
+      total: series.length,
+      series
     });
 
   } catch (error) {
@@ -78,7 +63,7 @@ export default async function handler(req, res) {
     console.error(error);
 
     return res.status(500).json({
-      error: "Erreur films",
+      error: "Erreur séries",
       details: error.message
     });
 

4587a6d84a87bd083f034728598c8c13d30b8a2d
EVRARDKODIA771 <kodia.evrard@outlook.com>
2026-06-17 00:02:50 +0000

    Update main-extended.jsx

--- src/main-extended.jsx
+++ src/main-extended.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Extended from "./components/extended";
+import Extended from "./components/serieFilms";
 
 ReactDOM.createRoot(
   document.getElementById("root")
